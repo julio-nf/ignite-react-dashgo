36e30099668079cd855ae90585bd801f48f0d473
Julio Novais <julio.novais@outlook.com>
2021-04-27 13:43:25 -0300

    feat: added user control pages

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
+import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
+import { Sidebar } from '../../components/Sidebar';
+
+export default function UserLists() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW="1480" mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading fontSize="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<RiAddLine fontSize="20" />}
+            >
+              Criar novo usuário
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Julio Novais</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      julio.novais@outlook.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>27 de abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<RiPencilLine fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
